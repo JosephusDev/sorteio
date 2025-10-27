8bb2db7d98b0ba369b8904da0090624f65263719
Seu Nome <enokimatumona0906@gmail.com>
2025-10-27 10:24:50 +0100

    updated

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ScrollView, TouchableOpacity } from "react-native";
+import { View, ScrollView, TouchableOpacity, StatusBar } from "react-native";
 import { LogOutIcon, UserStrokeIcon, HelpStrikeIcon } from "@/assets/icons";
 import { Text } from "@/components/Text";
 import { ProfilePhoto } from "@/components/ProfilePhoto";
@@ -54,14 +54,18 @@ export function Profile({
   );
 
   return (
-    <View className="flex-1 bg-white">
-      {/* Header do Perfil - REMOVIDO O BG AZUL */}
-      <View className="items-center pt-8 pb-4 px-6 bg-white">
-        <ProfilePhoto url={avatarUrl} />
-        <Text className="text-2xl font-urbanist-bold text-gray-800 text-center mt-4">
+    <View className="flex-1 relative">
+      <StatusBar barStyle={"light-content"} backgroundColor={"#4D5DFA"} />
+      <View className="flex-1 bg-white">
+
+      <View className="items-center pt-16 pb-4 px-12 bg-primary">
+       <View className="bg-white absolute rounded-full mt-24 mr-60">
+          <ProfilePhoto url={avatarUrl}  />
+        </View>
+        <Text className="text-2xl font-urbanist-bold text-white text-center mt-4 -mr-40">
           {userName}
         </Text>
-        <Text className="text-gray-600 mt-1 text-center font-urbanist-semiBold">
+        <Text className="text-blue-100 mt-1 text-center font-urbanist-semiBold -mr-40">
           +244 {userPhone.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
         </Text>
       </View>
@@ -96,7 +100,9 @@ export function Profile({
             isDestructive={true}
           />
         </View>
+       
       </ScrollView>
+      </View>
     </View>
   );
 }
